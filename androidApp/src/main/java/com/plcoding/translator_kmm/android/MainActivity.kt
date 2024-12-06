3391fb4b7e292709936228c604ba28d18a0d93d9
Marlon Escobar <escobar.marlon@gmail.com>
2024-12-06 17:03:19 -0500

    wip -> just testing2

--- androidApp/src/main/java/com/plcoding/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/plcoding/translator_kmm/android/MainActivity.kt
@@ -61,6 +61,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //test
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
